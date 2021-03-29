51370f3f44f8abb4011f35926f59d94ed0840d23
xw <15268201404@163.com>
2021-03-29 12:53:23 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/azkaban/AzkabanTest.java
@@ -0,0 +1,27 @@
+package com.atguigu.azkaban;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * @author pati_xw
+ * @create 2021-03-26 13:53
+ */
+public class AzkabanTest {
+
+    public static void main(String[] args) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("/opt/module/datas/output.txt");
+            fos.write("this is a java progress".getBytes());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
